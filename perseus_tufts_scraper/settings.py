593d26314bc5350900924268d1d5cea387dcde3c
paulhan97 <paul.seungoh.han@gmail.com>
2025-04-12 04:50:58 -0700

    Update settings to use pipeline

--- perseus_tufts_scraper/settings.py
+++ perseus_tufts_scraper/settings.py
@@ -63,9 +63,9 @@ DOWNLOADER_MIDDLEWARES = {
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "perseus_tufts_scraper.pipelines.PerseusTuftsScraperPipeline": 300,
-#}
+ITEM_PIPELINES = {
+   "perseus_tufts_scraper.pipelines.TextStructurePipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
